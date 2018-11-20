3ab0ee76d22363a214beb93ef6db9069c792cc19
Swati Rastogi <srastogi@conviva.com>
2018-11-20 23:31:37 +0530

    Added different ways of BST traversal

--- com/ds/saggi/VerifyBST.java
+++ com/ds/saggi/VerifyBST.java
@@ -43,7 +43,16 @@ public class VerifyBST {
         tree.root.left.right = new Node(7);
         tree.root.right = new Node(10);
 
-        System.out.println("If the tree is Binary tree : " + checkIfBST(tree.root));
+        boolean isBST = checkIfBST(tree.root);
+        System.out.println("If the tree is Binary tree : " + isBST);
+        if(isBST) {
+            System.out.println("\nPre-order traversal : ");
+            preOrderTraversal(tree.root);
+            System.out.println("\nIn-order traversal : ");
+            inOrderTraversal(tree.root);
+            System.out.println("\nPost-order traversal : ");
+            postOrderTraversal(tree.root);
+        }
     }
 
     private static boolean checkIfBST(Node node) {
@@ -52,11 +61,32 @@ public class VerifyBST {
 
     private static boolean checkIfBSTUtil(Node root, int min, int max) {
         if(root == null) return true;
-        System.out.println("processing node " + root.value);
 
         int key = root.value;
         if((key < min) || (key > max)) return false;
 
         return ((root.left == null || checkIfBSTUtil(root.left, min, key)) && (root.right == null || checkIfBSTUtil(root.right, key + 1, max)) );
+    }
+
+    private static void inOrderTraversal(Node root) {
+        if(root == null) return;
+        inOrderTraversal(root.left);
+        System.out.print(root.value + " ");
+        inOrderTraversal(root.right);
+    }
+
+    private static void preOrderTraversal(Node root) {
+        if(root == null) return;
+
+        System.out.print(root.value + " ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+
+    private static void postOrderTraversal(Node root) {
+        if(root == null) return;
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.print(root.value + " ");
     }
 }
